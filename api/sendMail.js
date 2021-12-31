2a41248ca7ed229d31dd5b389c996a9ef82932f0
Aleksey <alekseyhonchar@gmail.com>
2021-12-31 16:19:23 +0200

    a

--- api/sendMail.js
+++ api/sendMail.js
@@ -11,7 +11,7 @@ async function formSubmit(formData) {
   const data = new Date();
   return sendMail({
     from,
-    to: toMail,
+    to: 'alekseyhonchar@gmail.com',
     subject: 'New user',
     html: sanitizer(
       `<ul><li>${formData.email}</li><li>${formData.name}</li></ul><br>${data}`
@@ -29,7 +29,7 @@ const rateLimit = (ip, limit = 3) => {
 
 function getTransporter() {
   return nodemailer.createTransport({
-    host: hostMail,
+    host: 'smtp.gmail.com',
     port: 587,
     secure: false, // upgrade later with STARTTLS
     auth: {
